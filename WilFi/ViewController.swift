9554f3d3b70d820080951527534970681015fd2b
adcrops-net <devid@8crops.com>
2017-10-13 16:25:16 +0900

    Initial Commit

---
+++ WilFi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WilFi
+//
+//  Created by Tatsuya Uemura on 2017/10/13.
+//  Copyright © 2017年 Fancs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
